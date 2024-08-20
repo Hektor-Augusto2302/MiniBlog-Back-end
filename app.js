7def5d5036dcd7d6258a4e7f2039818e44eaeb0d
Hektor-Augusto <hektoraugusto.s@gmail.com>
2024-08-20 09:41:23 -0300

    initial settings

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config()
+
+const conn = require("./config/db.js");
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+const port = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.use(cors({credentials: true, origin: "http://localhost:3000"}));
+
+conn();
+
+const router = require('./routes/Router.js');
+
+app.use(router);
+
+app.listen(port, () => {
+    console.log(`App Rodando na porta ${port}`);
+});
