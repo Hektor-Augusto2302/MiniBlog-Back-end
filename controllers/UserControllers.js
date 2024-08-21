2311ca99c7a68c38cad9a899907f2b48c8ffcdb8
Hektor-Augusto <hektoraugusto.s@gmail.com>
2024-08-21 09:34:32 -0300

    feat(initial user registration test)

---
+++ controllers/UserControllers.js
@@ -0,0 +1,47 @@
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.JWT_SECRET;
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, jwtSecret, {
+        expiresIn: "7d",
+    });
+};
+
+const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(422).json({ errors: ['Este email já está em uso'] });
+        }
+
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        res.status(201).json({
+            _id: newUser._id,
+            token: generateToken(),
+        });
+    } catch (error) {
+        console.error('Erro ao registrar usuário:', error);
+        res.status(500).json({ error: 'Erro interno do servidor' });
+    }
+};
+
+module.exports = {
+    registerUser,
+    // registerAdminUser,
+    // login,
+    // update,
+    // getCurrentUser,
+    // getUserById,
+}
---
+++ routes/UserRoutes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+
+const { registerUser } = require("../controllers/UserControllers");
+
+router.post("/register/user", registerUser);
+
+module.exports = router;
